4f3e8dab80c44df2e2c5d7b232590ec4500e39ad
kabramovich <wieker@gmail.com>
2020-02-13 23:57:35 +0200

    two-way comm

--- rf_uart.c
+++ rf_uart.c
@@ -21,50 +21,62 @@ void clocks_start( void )
 }
 
 static nrf_esb_payload_t        rx_payload;
+int count = 0;
+int latest = 0;
+
+void advertise();
+
+void wait_and_blink();
+
+static nrf_esb_config_t nrf_esb_config = NRF_ESB_DEFAULT_CONFIG;
+static nrf_esb_payload_t tx_payload, rx_payload;
+static volatile bool radio_tx_busy = false, radio_started = false;
 
 void nrf_esb_event_handler(nrf_esb_evt_t const * p_event)
 {
     switch (p_event->evt_id)
     {
         case NRF_ESB_EVENT_TX_SUCCESS:
-            nrfx_uart_tx(&m_uart.uart, (uint8_t  *) "tx \r\n", 5);
+            radio_tx_busy = false;
+            nrf_esb_disable();
+            nrf_esb_config.mode = NRF_ESB_MODE_PRX;
+            nrf_esb_init(&nrf_esb_config);
+            nrf_esb_start_rx();
             break;
         case NRF_ESB_EVENT_TX_FAILED:
-            nrfx_uart_tx(&m_uart.uart, (uint8_t  *) "fail\r\n", 6);
-            (void) nrf_esb_flush_tx();
-            (void) nrf_esb_start_tx();
+            radio_tx_busy = false;
             break;
         case NRF_ESB_EVENT_RX_RECEIVED:
             if (nrf_esb_read_rx_payload(&rx_payload) == NRF_SUCCESS)
             {
                 nrfx_uart_tx(&m_uart.uart, (uint8_t  *) rx_payload.data, rx_payload.length);
-
-                NRF_P0->OUTSET = 1 << 20 | 1 << 22;
-                NRF_P0->OUTCLR = 1 << 18 | 1 << 19;
-                nrf_delay_ms(100);
-
-                NRF_P0->OUTSET = 1 << 18 | 1 << 19;
-                NRF_P0->OUTCLR = 1 << 20 | 1 << 22;
-                nrf_delay_ms(100);
+                int rcvr = rx_payload.data[0] - '0';
+                if (rcvr > latest) {
+                    count ++;
+                    latest = rcvr;
+                }
             }
             break;
     }
 }
 
-
-uint32_t esb_init_rx(void )
+uint32_t radio_init(uint16_t network_id)
 {
     uint32_t err_code;
-    uint8_t base_addr_0[4] = {0xE7, 0xE7, 0xE7, 0xE7};
+    uint8_t base_addr_0[4] = {0xC5, 0xE7, 0xE7, 0x3A};
     uint8_t base_addr_1[4] = {0xC2, 0xC2, 0xC2, 0xC2};
     uint8_t addr_prefix[8] = {0xE7, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8 };
-    nrf_esb_config_t nrf_esb_config         = NRF_ESB_DEFAULT_CONFIG;
-    nrf_esb_config.payload_length           = 8;
+
+    nrf_esb_config.payload_length           = 250;
+    nrf_esb_config.tx_mode                  = NRF_ESB_TXMODE_AUTO;
     nrf_esb_config.protocol                 = NRF_ESB_PROTOCOL_ESB_DPL;
     nrf_esb_config.bitrate                  = NRF_ESB_BITRATE_2MBPS;
     nrf_esb_config.mode                     = NRF_ESB_MODE_PRX;
     nrf_esb_config.event_handler            = nrf_esb_event_handler;
-    nrf_esb_config.selective_auto_ack       = false;
+    nrf_esb_config.selective_auto_ack       = true;
+
+    base_addr_0[1] ^= (network_id & 0xFF);
+    base_addr_0[2] ^= (network_id >> 8);
 
     err_code = nrf_esb_init(&nrf_esb_config);
     VERIFY_SUCCESS(err_code);
@@ -78,39 +90,35 @@ uint32_t esb_init_rx(void )
     err_code = nrf_esb_set_prefixes(addr_prefix, 8);
     VERIFY_SUCCESS(err_code);
 
+    err_code = nrf_esb_set_rf_channel(network_id % 64 + 4);
+    VERIFY_SUCCESS(err_code);
+
+    err_code = nrf_esb_start_rx();
+    VERIFY_SUCCESS(err_code);
+
+    radio_started = true;
+
     return err_code;
 }
 
-
-uint32_t esb_init_tx( void )
+bool radio_packet_send(uint8_t *packet, uint32_t packet_length)
 {
-    uint32_t err_code;
-    uint8_t base_addr_0[4] = {0xE7, 0xE7, 0xE7, 0xE7};
-    uint8_t base_addr_1[4] = {0xC2, 0xC2, 0xC2, 0xC2};
-    uint8_t addr_prefix[8] = {0xE7, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8 };
-
-    nrf_esb_config_t nrf_esb_config         = NRF_ESB_DEFAULT_CONFIG;
-    nrf_esb_config.protocol                 = NRF_ESB_PROTOCOL_ESB_DPL;
-    nrf_esb_config.retransmit_delay         = 600;
-    nrf_esb_config.bitrate                  = NRF_ESB_BITRATE_2MBPS;
-    nrf_esb_config.event_handler            = nrf_esb_event_handler;
-    nrf_esb_config.mode                     = NRF_ESB_MODE_PTX;
-    nrf_esb_config.selective_auto_ack       = false;
-
-    err_code = nrf_esb_init(&nrf_esb_config);
-
-    VERIFY_SUCCESS(err_code);
-
-    err_code = nrf_esb_set_base_address_0(base_addr_0);
-    VERIFY_SUCCESS(err_code);
-
-    err_code = nrf_esb_set_base_address_1(base_addr_1);
-    VERIFY_SUCCESS(err_code);
-
-    err_code = nrf_esb_set_prefixes(addr_prefix, NRF_ESB_PIPE_COUNT);
-    VERIFY_SUCCESS(err_code);
-
-    return err_code;
+    if(radio_started)
+    {
+        while(radio_tx_busy);
+        nrf_esb_stop_rx();
+        nrf_esb_disable();
+        nrf_esb_config.mode = NRF_ESB_MODE_PTX;
+        nrf_esb_init(&nrf_esb_config);
+        nrf_esb_start_tx();
+        memcpy(tx_payload.data, packet, packet_length);
+        tx_payload.length = packet_length;
+        tx_payload.pipe = 0;
+        tx_payload.noack = true;
+        if(nrf_esb_write_payload(&tx_payload) == NRF_SUCCESS) radio_tx_busy = true;
+        return true;
+    }
+    return false;
 }
 
 int main() {
@@ -119,43 +127,31 @@ int main() {
 
     clocks_start();
 
-    /*esb_init_tx();
-    nrf_esb_payload_t tx_payload;
+    radio_init(1);
 
     while (true)
     {
-        memcpy(tx_payload.data, "zz\r\n", 4);
-        tx_payload.length = 4;
-        tx_payload.noack = false;
-        tx_payload.pipe = 0;
-        if (nrf_esb_write_payload(&tx_payload) == NRF_SUCCESS)
-        {
-            nrfx_uart_tx(&m_uart.uart, (uint8_t  *) "kk \r\n", 5);
-        }
+        wait_and_blink();
 
-        NRF_P0->OUTSET = 1 << 20 | 1 << 22;
-        NRF_P0->OUTCLR = 1 << 18 | 1 << 19;
-        nrf_delay_ms(300);
-
-        NRF_P0->OUTSET = 1 << 18 | 1 << 19;
-        NRF_P0->OUTCLR = 1 << 20 | 1 << 22;
-        nrf_delay_ms(300);
-    }*/
-
-    esb_init_rx();
-
-    nrf_esb_start_rx();
-
-    while (1) {
-        //NRF_P0->OUTSET = 1 << 17 | 1 << 20;
-        //NRF_P0->OUTCLR = 1 << 18 | 1 << 19;
-        nrf_delay_ms(300);
-
-        //NRF_P0->OUTSET = 1 << 18 | 1 << 19;
-        //NRF_P0->OUTCLR = 1 << 17 | 1 << 20;
-        nrf_delay_ms(300);
-
-        nrfx_uart_tx(&m_uart.uart, (uint8_t  *) "ok \r\n", 5);
+        advertise();
     }
+}
+
+void wait_and_blink() {
+    NRF_P0->OUTSET = 1 << 20 | 1 << 22;
+    NRF_P0->OUTCLR = 1 << 18 | 1 << 19;
+    nrf_delay_ms(300);
+
+    NRF_P0->OUTSET = 1 << 18 | 1 << 19;
+    NRF_P0->OUTCLR = 1 << 20 | 1 << 22;
+    nrf_delay_ms(300);
+}
+
+void advertise() {
+    char data[4];
+    data[0] = '0' + count + 1;
+    data[1] = '\r';
+    data[2] = '\n';
+    radio_packet_send((uint8_t *) data, 3);
 }
 
