e7b52734f64d5ed92e215924ef9c09f830db9aa1
kabramovich <wieker@gmail.com>
2020-03-12 01:47:44 +0200

    trying to debug UART

--- main.c
+++ main.c
@@ -4,12 +4,12 @@ int main() {
     NRF_P0->DIRSET = 1 << 17 | 1 << 18 | 1 << 19 | 1 << 20;
 
     while (1) {
-        NRF_P0->OUTSET = 1 << 17 | 1 << 20;
+        NRF_P0->OUTSET = 1 << 30 | 1 << 20;
         NRF_P0->OUTCLR = 1 << 18 | 1 << 19;
-        nrf_delay_ms(300);
+        nrf_delay_ms(3000);
 
         NRF_P0->OUTSET = 1 << 18 | 1 << 19;
-        NRF_P0->OUTCLR = 1 << 17 | 1 << 20;
-        nrf_delay_ms(300);
+        NRF_P0->OUTCLR = 1 << 30 | 1 << 20;
+        nrf_delay_ms(3000);
     }
 }
