957de57b81e47c5aa0ff409299c12d6147fab056
kabramovich <wieker@gmail.com>
2020-03-15 02:42:50 +0200

    trying to debug blink on the new board

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <nrf_delay.h>
 
 int main() {
-    NRF_P0->DIRSET = 1 << 17 | 1 << 18 | 1 << 19 | 1 << 20;
+    NRF_P0->DIRSET = 1 << 24 | 1 << 25 | 1 << 26 | 1 << 27;
 
     while (1) {
-        NRF_P0->OUTSET = 1 << 30 | 1 << 20;
-        NRF_P0->OUTCLR = 1 << 18 | 1 << 19;
+        NRF_P0->OUTSET = 1 << 24 | 1 << 25;
+        NRF_P0->OUTCLR = 1 << 26 | 1 << 27;
         nrf_delay_ms(3000);
 
-        NRF_P0->OUTSET = 1 << 18 | 1 << 19;
-        NRF_P0->OUTCLR = 1 << 30 | 1 << 20;
+        NRF_P0->OUTSET = 1 << 26 | 1 << 27;
+        NRF_P0->OUTCLR = 1 << 24 | 1 << 25;
         nrf_delay_ms(3000);
     }
 }
