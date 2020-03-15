323a3542baac91f6bd67480ffca7d3736ce62202
kabramovich <wieker@gmail.com>
2020-03-15 03:00:12 +0200

    trying to debug UART on the new board

--- uart.c
+++ uart.c
@@ -12,7 +12,7 @@ nrf_drv_uart_t m_uart = NRF_DRV_UART_INSTANCE(0);
 void uart_init()
 {
     nrf_drv_uart_config_t config = NRF_DRV_UART_DEFAULT_CONFIG;
-    config.pseltxd  = 11;
+    config.pseltxd  = 29;
     config.pselrxd  = NRF_UART_PSEL_DISCONNECTED;
     config.pselcts  = NRF_UART_PSEL_DISCONNECTED;
     config.pselrts  = NRF_UART_PSEL_DISCONNECTED;
@@ -23,17 +23,17 @@ void uart_init()
 }
 
 int main() {
-    NRF_P0->DIRSET = 1 << 17 | 1 << 18 | 1 << 19 | 1 << 20;
+    NRF_P0->DIRSET = 1 << 24 | 1 << 25 | 1 << 26 | 1 << 27;
     uart_init();
 
     while (1) {
-        NRF_P0->OUTSET = 1 << 17 | 1 << 20;
-        NRF_P0->OUTCLR = 1 << 18 | 1 << 19;
-        nrf_delay_ms(300);
+        NRF_P0->OUTSET = 1 << 24 | 1 << 25;
+        NRF_P0->OUTCLR = 1 << 26 | 1 << 27;
+        nrf_delay_ms(3000);
 
-        NRF_P0->OUTSET = 1 << 18 | 1 << 19;
-        NRF_P0->OUTCLR = 1 << 17 | 1 << 20;
-        nrf_delay_ms(300);
+        NRF_P0->OUTSET = 1 << 26 | 1 << 27;
+        NRF_P0->OUTCLR = 1 << 24 | 1 << 25;
+        nrf_delay_ms(3000);
 
         nrfx_uart_tx(&m_uart.uart, (uint8_t  *) "ok \r\n", 5);
     }
